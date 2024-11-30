969ceee05f54598b9853b2e71c4ad698104f5fdd
Nicolau <nicolaucola@gmail.com>
2024-11-30 08:26:27 -0300

    Incluindo agents e tools, além de refatorando ice_breaker.py para utilizar funcoes auxiliares.

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,12 +1,16 @@
 import os
+from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers.string import StrOutputParser
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 
-if __name__ == "__main__":
-    print("Hello LangChain")
+
+def ice_breaker_with(name: str) -> str:
+    linkedin_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_url)
 
     summary_template = """Given a LinkdIn information {information} about a person from, I want to create:
      1- a short summary
@@ -19,8 +23,21 @@ if __name__ == "__main__":
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = sumary_prompt_template | llm | StrOutputParser()
+    
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/helio-cola/")
     
     res = chain.invoke(input={"information": linkedin_data})
     
     print(res)
+
+
+if __name__ == "__main__":
+    
+    load_dotenv()
+    print("Ice Breaker Enter")
+
+    ice_breaker_with(name="Eden Marco")
+
+    
+
+
